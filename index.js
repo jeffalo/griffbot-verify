89e52308a22824e891e4e49567839d86e02ffbe3
jeffalo <40470736+jeffalo@users.noreply.github.com>
2021-07-21 21:47:47 +0200

    add remove

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on('message', async message => {
   } else if (message.content.startsWith('g!done')) {
     let scratchResponse = await fetch('https://clouddata.scratch.mit.edu/logs?projectid=554914758&limit=40&offset=0').then(r => r.json())
     // check if the code is in the array of cloud actions
-    if(!codes.filter(i => i.discord === message.author.id)[0]) return message.author.send(`sorry. you failed verification. please try again.`)
+    if (!codes.filter(i => i.discord === message.author.id)[0]) return message.author.send(`sorry. you failed verification. please try again.`)
     let code = codes.filter(i => i.discord === message.author.id)[0].code
     let cloudUpdate = scratchResponse.find(i => i.value == code.toString())
     // if the code is in the array, respond with their username
@@ -34,21 +34,31 @@ client.on('message', async message => {
       let logChannel = server.channels.cache.get(process.env.LOG_CHANNEL_ID)
       logChannel.send(`${message.author.username} (${message.author.id}) verified as ${cloudUpdate.user}`)
 
-      let rawUsers =  await fs.promises.readFile('./users.json', 'utf8')
+      let rawUsers = await fs.promises.readFile('./users.json', 'utf8')
       let users = JSON.parse(rawUsers)
       let user = users.find(i => i.discord == message.author.id)
-      if(user) {
-        if(user.scratch.includes(cloudUpdate.user)) {
+      if (user) {
+        if (user.scratch.includes(cloudUpdate.user)) {
           return message.author.send(`you are already verified as ${cloudUpdate.user}.`)
         } else {
           user.scratch.push(cloudUpdate.user)
+          user.updated = Date.now()
         }
       } else {
         // we add the user to the array
-        users.push({ discord: message.author.id, scratch: [cloudUpdate.user] })
+        users.push({ discord: message.author.id, scratch: [cloudUpdate.user], updated: Date.now() })
       }
       await fs.promises.writeFile('./users.json', JSON.stringify(users, null, 2), 'utf8')
-      message.author.send(`epic! you are now verified as ${cloudUpdate.user}. you can verify as many times as you want to add alt accounts.`)
+      message.author.send({
+        embed: {
+          "title": "Verification Confirmation",
+          "description": `You have added **${cloudUpdate.user}** as a scratch account. \n\n**Current list of accounts:**\n${users.find(i => i.discord == message.author.id).scratch.map(i => '- ' + i).join('\n')}\n\nYou can verify more accounts by typing g!verify.`,
+          "color": '#00a9c0',
+          "thumbnail": {
+            "url": `https://my-ocular.jeffalo.net/api/user/${cloudUpdate.user}/picture`
+          }
+        }
+      })
       codes = codes.filter(i => i.code !== code)
     } else {
       message.author.send(`sorry. you failed verification. please try again.`)
@@ -56,13 +66,79 @@ client.on('message', async message => {
   } else if (message.content.startsWith('g!whois')) {
     // if the user has the moderator role
     if (!message.member.roles.cache.get(process.env.MODERATOR_ROLE_ID)) return
-    let rawUsers =  await fs.promises.readFile('./users.json', 'utf8')
+    let rawUsers = await fs.promises.readFile('./users.json', 'utf8')
     let users = JSON.parse(rawUsers)
     let user = users.find(i => i.discord == message.mentions.users.first())
-    if(user) {
-      message.channel.send(`${user.scratch.join(', ')}`)
+    if (user) {
+      message.channel.send({
+        embed: {
+          "title": `Verification Status (${message.mentions.users.first().tag})`,
+          "description": `**Current list of accounts:**\n${users.find(i => i.discord == message.mentions.users.first()).scratch.map(i => '- ' + i).join('\n')}\n\nLast updated: <t:${Math.floor(user.updated/1000)}:R>.`,
+          "color": '#00a9c0',
+          "thumbnail": {
+            "url": message.mentions.users.first().displayAvatarURL()
+          }
+        }
+      })
     } else {
       message.channel.send('isnt verified.')
+    }
+  } else if (message.content.startsWith('g!scratchwhois')) {
+    // get the discord accounts linked to a scratch username
+    if (!message.member.roles.cache.get(process.env.MODERATOR_ROLE_ID)) return
+    let rawUsers = await fs.promises.readFile('./users.json', 'utf8')
+    let users = JSON.parse(rawUsers)
+    let linkedUsers = users.filter(i => i.scratch.includes(message.content.split(' ')[1]))
+    if (linkedUsers.length) {
+      let discords = linkedUsers.map(i=>`<@${i.discord}>`).join('\n')
+      message.channel.send({
+        embed: {
+          "title": `Discord accounts linked for ${message.content.split(' ')[1]}`,
+          "description": `**List of discord accounts:**\n${discords}`,
+          "color": '#00a9c0',
+        }
+      })
+    } else {
+      message.channel.send(`${message.content.split(' ')[1]} isnt linked to any discords.`)
+    }
+  } else if (message.content.startsWith('g!remove')) {
+    // if the user has one linked scratch account, remove them completely
+    // otherwise, remove only one linked scratch account
+    if (!message.member.roles.cache.get(process.env.MODERATOR_ROLE_ID)) return
+    let rawUsers = await fs.promises.readFile('./users.json', 'utf8')
+    let users = JSON.parse(rawUsers)
+
+    let discordID = message.mentions.users.first().id
+    let scratchUsername = message.content.split(' ')[2]
+    let user = users.find(i => i.discord == discordID)
+    if (user) {
+      // if the user has a linked scratch account with the username
+      if (user.scratch.includes(scratchUsername)) {
+        // remove the linked scratch account
+        user.scratch = user.scratch.filter(i => i !== scratchUsername)
+        user.updated = Date.now()
+
+        // if the user has now has no linked scratch accounts, remove them completely
+        if (user.scratch.length == 0) {
+          users = users.filter(i => i.discord != discordID)
+        }
+
+        await fs.promises.writeFile('./users.json', JSON.stringify(users, null, 2), 'utf8')
+        message.channel.send({
+          embed: {
+            "title": `Verification Status (${message.mentions.users.first().tag})`,
+            "description": `**Current list of accounts:**\n${users.find(i => i.discord == message.mentions.users.first()).scratch.map(i => '- ' + i).join('\n')}\n\nLast updated: <t:${Math.floor(user.updated/1000)}:R>.`,
+            "color": '#00a9c0',
+            "thumbnail": {
+              "url": message.mentions.users.first().displayAvatarURL()
+            }
+          }
+        })
+      } else {
+        message.channel.send(`${scratchUsername} isnt linked to ${message.mentions.users.first().tag}.`)
+      }
+    } else {
+      message.channel.send(`${message.mentions.users.first().tag} isnt linked to any scratch accounts.`)
     }
   }
 })
